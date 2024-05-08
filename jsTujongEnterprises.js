3d493b52bf8bb650baf49ce3ce140a6a169490cb
whiteumbrella <84945956+JphOrq@users.noreply.github.com>
2024-05-08 20:58:00 +0800

    Update jsTujongEnterprises.js

--- jsTujongEnterprises.js
+++ jsTujongEnterprises.js
@@ -3,3 +3,8 @@ $(document).ready(function () {
     interval: 5000,
   });
 });
+
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+document.getElementById("currentYear").textContent = currentYear;
